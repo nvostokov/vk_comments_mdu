d2d0843e9131f9978cdd765926ccbdf07eaadc12
nvostokov <103598346+nvostokov@users.noreply.github.com>
2023-02-12 22:14:06 +0300

    Add files via upload

---
+++ js/object_shishov.js
@@ -0,0 +1,90 @@
+
+$('#shishov').on('click', loadComments); //запуск по кнопке
+/* $('#load').ready(loadComments); */
+    
+function getUrl(method, params) {
+    if (!method) throw new Error('Вы не указали метод');
+    params = params || {}; //если параметры не определены, то пустой объект
+    params['access_token'] = token;
+    return 'https://api.vk.com/method/' + method + '?' +  $.param(params);
+}
+    
+function sendRequest(method, params, func) {
+    $.ajax({
+        url: getUrl(method, params),
+        method: 'GET',
+        dataType: 'JSONP',
+        success: func
+    });
+}
+    
+function loadComments() {
+    sendRequest('execute.newShishov', {
+            v: 5.131
+        }, 
+            function (data) {
+                console.log(data.response);
+                array = data.response;
+                array.sort(function (a, b) {
+                    if (a.date < b.date) {
+                        return 1;
+                      }
+                      if (a.date > b.date) {
+                        return -1;
+                      }
+                      // a должно быть равным b
+                      return 0;
+                })
+				
+				console.log(array)
+
+                drawComments(array);
+    });
+}
+    
+        function drawComments(comments) {
+            let html = '';
+    
+            for (let i = 0; i < comments.length; i++) {
+                
+                let f = comments[i];
+    
+                //преобразование даты, сначала в GMT, потом в русское наименование
+   
+                newDate = new Date(f.date*1000);
+    
+                newDate.toString;
+    
+                let a = new Date(newDate);
+    
+                let timeNext = [
+                    addLeadZero(a.getHours()),
+                    addLeadZero(a.getMinutes()),
+                    ].join(':');
+    
+                let dateNext = [
+                    addLeadZero(a.getDate()),
+                    addLeadZero(a.getMonth() + 1),
+                    a.getFullYear()].join('.');
+    
+                function addLeadZero(val) {
+                if (+val < 10) return '0' + val;
+                return val;
+                };
+    
+                //вывод в html
+    
+                html += '<li>' + 
+                            '<div>'
+                                + '<p class="comments__date">' + dateNext + ' ' + timeNext + '</p>' 
+                                +'<h4>' + f.text + '</h4>'
+                            +'</div>'
+                            + '<div class="comments__link">'
+                                + '<a target="_blank" href="https://vk.com/' +/*  urlDomain2 + '?w=wall' */ 'wall'+ f.owner_id + '_' + f.id + '">' 
+                                + 'ссылка на пост' + '</a>' 
+                            +'</div>'
+                    + '</li>';
+            }
+    
+            $('ul').html(html);
+        }
---
+++ js/query.js
@@ -0,0 +1,14 @@
+/* let queries = [
+    "МДУ",
+    "Мезенское",
+    "дороги",
+    "мезенцы",
+    "дорожники",
+    "тротуар",
+]
+
+
+/* let queries = prompt('Введите ключевое слово:') */
+
+
+let token = 'vk1.a.5xW9qQsYsSzi_iDRUVFE1whP8fT0vK8L_O-hBTTv2yLUVUhdV6E73RjBDDExKCKJJxBcwVHleAVV-GSw8bLYB7-JjuBdIl58pKvSC1sfY6AcUrlyAv7oWv_wf2JpmGXlIwLhDogtYJk9P_pdt4Uex-n7CEk56ucWF0eD68NceevrBs7RCPXEWsqsfJ3GAiuUlHIOSPtoPQ05y5hwvj2nvA';
